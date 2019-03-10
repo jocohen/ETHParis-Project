f0e2c0a72763f262882c2680d37ea771281a584b
rfibigr <rfibigr@student.42.fr>
2019-03-10 03:19:56 +0100

    front to do

--- dapp/server.js
+++ dapp/server.js
@@ -2,13 +2,16 @@ const express = require('express');
 const app = express();
 const Web3 = require('web3');
 
-
 app.use(express.static('client'));
 app.use(express.static('build/contracts'));
-
 app.get('/', function(req, res){
 	res.sendFile(`${__dirname}/client/index.html`);
-  });
+});
+
+app.get('/create', function(req, res){
+	res.sendFile(`${__dirname}/client/create.html`);
+});
+
 app.get('*', function(req, res){
 	res.status(404);
 	res.send("404 Page not found");
